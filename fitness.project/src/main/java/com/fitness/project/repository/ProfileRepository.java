d80a572685c9c8f5029ad7e3c66e4e537bf495f7
abhijith sreekumar <sreekumar.abhijith93@gmail.com>
2024-09-26 20:49:42 +0530

    reverted
    reverted

--- fitness.project/src/main/java/com/fitness/project/repository/ProfileRepository.java
+++
@@ -1,9 +0,0 @@
-package com.fitness.project.repository;
-
-import com.fitness.project.entities.UserProfile;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ProfileRepository extends CrudRepository<UserProfile, String> {
-
-
-}
